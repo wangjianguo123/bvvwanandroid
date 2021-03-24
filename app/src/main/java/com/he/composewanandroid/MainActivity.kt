a3a70b5a253f599c8cbfa9de79f8f41ff4021890
wangjianguo123 <2961371926@qq.com>
2021-03-24 23:36:31 +0800

    初始化项目

---
+++ app/src/main/java/com/he/composewanandroid/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.he.composewanandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.he.composewanandroid.ui.theme.ComposeWanAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeWanAndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeWanAndroidTheme {
+        Greeting("Android")
+    }
+}
